6afeea34d96b5677df962223f1896c4b6f162d48
Pham Duc Dat <phamducdat@Dat-Phams-MacBook-Pro.local>
2024-03-12 15:34:45 +0700

    fix clo score

--- src/Admin/ExamTeams/ExamTeamsScore.js
+++ src/Admin/ExamTeams/ExamTeamsScore.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Button, Upload, message } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import { UploadOutlined, DownloadOutlined } from '@ant-design/icons';
 import ExamTeamsScoreTable from './ExamTeamsScoreTable';
 import { setDataListExamteams } from '../../Redux/actions/actionExamteams';
 import * as XLSX from 'xlsx';
@@ -21,7 +21,7 @@ export default function ExamTeamsScore() {
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
         const excelData = XLSX.utils.sheet_to_json(worksheet);
-        setFileData(excelData); // Save the data to state
+        setFileData(excelData); 
         message.success(`${file.name} file processed successfully`);
       } catch (error) {
         console.error("Error reading the Excel file: ", error);
@@ -33,61 +33,22 @@ export default function ExamTeamsScore() {
       message.error("There was an issue reading the Excel file.");
     };
     reader.readAsBinaryString(file);
-    // The `return false` here might be unnecessary, as the function doesn't need to return anything for `customRequest`.
   };
 
   const beforeUpload = (file) => {
     const isExcel = file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || file.type === "application/vnd.ms-excel";
     if (!isExcel) {
       message.error(`${file.name} is not an excel file.`);
-      return Upload.LIST_IGNORE; // This should be the return value if the file is not an Excel file.
+      return Upload.LIST_IGNORE; 
     }
-    return true; // Return true to allow the file upload process to continue.
+    return true;
   };
 
-  // const handleSaveScores = async () => {
-  //   // Chuyển đổi dữ liệu từ Excel sang cấu trúc cần thiết cho API
-  //   const scoresForApi = fileData.map((student, index) => {
-  //     return {
-  //       id: student['No'].toString(), // Đảm bảo rằng 'id' là một chuỗi
-  //       examScore: {
-  //         No: student['No'],
-  //         studentID: student['Student ID'],
-  //         studentFirstName: student['Student first name'],
-  //         studentLastName: student['Student last name'],
-  //         score: [{
-  //           mcq_q1: student.mcq_q1,
-  //           mcq_q2: student.mcq_q2,
-  //           mcq_q3: student.mcq_q3,
-  //           mcq_q4: student.mcq_q4,
-  //           mcq_q5: student.mcq_q5,
-  //           wq_q1: student.wq_q1,
-  //           wq_q2: student.wq_q2,
-  //           note: student.Note
-  //         }]
-  //       }
-  //     };
-  //   });
-  
-  //   try {
-  //     // Gửi mỗi đối tượng điểm số lên API
-  //     for (const scoreData of scoresForApi) {
-  //       await examteamsServ.importExamTeamsScore({ examScore: scoreData.examScore });
-  //       // Đảm bảo rằng phương thức importExamTeamsScore được thiết kế để nhận và xử lý cấu trúc dữ liệu này
-  //     }
-  //     message.success('Exam scores have been successfully updated.');
-  //     // Làm mới dữ liệu trong Redux store nếu cần
-  //     dispatch(setDataListExamteams());
-  //   } catch (error) {
-  //     message.error('Failed to update exam scores.');
-  //     console.error('Error updating exam scores:', error);
-  //   }
-  // };
+
   const handleSaveScores = () => {
-    // Chuyển đổi dữ liệu từ Excel sang cấu trúc cần thiết
     const scoresForLocalStorage = fileData.map((student) => {
       return {
-        id: student['No'].toString(), // Đảm bảo rằng 'id' là một chuỗi
+        id: student['No'].toString(), 
         examScore: {
           No: student['No'],
           studentID: student['Student ID'],
@@ -107,9 +68,7 @@ export default function ExamTeamsScore() {
       };
     });
   
-    // Serialize và lưu trữ dữ liệu vào localStorage
     try {
-      // Lưu từng bản ghi dựa trên ID của sinh viên
       scoresForLocalStorage.forEach((record) => {
         localStorage.setItem(`examScore-${record.id}`, JSON.stringify(record.examScore));
       });
@@ -121,7 +80,12 @@ export default function ExamTeamsScore() {
     }
   };
   
-  
+  // const handleExportCLOsResult = () => {
+  //   const ws = XLSX.utils.json_to_sheet(fileData);
+  //   const wb = XLSX.utils.book_new();
+  //   XLSX.utils.book_append_sheet(wb, ws, "CLOsResult");
+  //   XLSX.writeFile(wb, "Ex_CLOsResult.xlsx");
+  // };
   return (
     <div className="container mx-auto">
       <h1 className="text-2xl">Exam Teams Scores</h1>
@@ -140,6 +104,19 @@ export default function ExamTeamsScore() {
       >
         Save Scores
       </Button>
+      {fileData.length > 0 && (
+        <a
+          href={`/assets/Ex_CLOsResult.xlsx`}
+          download="Ex_CLOsResult.xlsx"
+        >
+          <Button
+            icon={<DownloadOutlined />}
+            style={{ marginLeft: 8 }}
+          >
+            Export CLO Result
+          </Button>
+        </a>
+      )}
       <ExamTeamsScoreTable data={fileData} />
     </div>
   );
